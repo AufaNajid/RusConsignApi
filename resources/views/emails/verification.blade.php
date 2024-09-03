988034513dcb5680a165a6bb12960caf88a64d1a
Aufa Najid <nl2221171@gmail.com>
2024-09-03 14:42:35 +0700

    fix conflict

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -6,7 +6,7 @@
 <body>
 <h1>Hello, {{ $user->name }}!</h1>
 <p>Please click the link below to verify your email address:</p>
-<a href="{{ url('/verify-email-form?token=' . $token) }}" target="_blank">Verify Email</a>
+<a href="{{ url('/verify-email?token=' . $token) }}" target="_blank">Verify Email</a>
 <p>Thank you for registering with us!</p>
 </body>
 </html>
