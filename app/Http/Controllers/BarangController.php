25428465f6c444a37bbc922493b53d7d091c9294
Aufa Najid <nl2221171@gmail.com>
2024-09-04 02:58:29 +0700

    fix conflict

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -307,7 +307,7 @@ class BarangController extends Controller
 
     public function show($id)
     {
-        $barang = Barang::with('category:id,name', 'mitra:id,nama_lengkap,nama_toko,jumlah_product,jumlah_jasa,pengikut,penilaian,no_whatsapp,email,image_profile,profile')->find($id);
+        $barang = Barang::with('category:id,name', 'mitra:id,nama_lengkap,nama_toko,jumlah_product,jumlah_jasa,pengikut,penilaian,no_whatsapp,email,image_profile,profileImage')->find($id);
 
         if (!$barang) {
             return response()->json(['message' => 'Barang tidak ditemukan'], 404);
@@ -351,7 +351,7 @@ class BarangController extends Controller
                 'penilaian' => $barang->mitra->penilaian,
                 'no_whatsapp' => $barang->mitra->no_whatsapp,
                 'image_profile' => $barang->mitra->image_profile, // Menambahkan image_profile
-                'profile_image' => $barang->mitra->profile->image_profile ?? null // Mengambil image_profile dari relasi profile di mitra
+                'profile_image' => $barang->mitra->profileImage->image_profile ?? null // Mengambil image_profile dari relasi profile di mitra
             ]
         ];
 
