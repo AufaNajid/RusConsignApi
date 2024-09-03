e6680effdab9a72556ef4d65e6bd4c16deaee38c
Aufa Najid <nl2221171@gmail.com>
2024-09-03 13:25:31 +0700

    fix conflict

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,6 +15,50 @@ use Symfony\Component\HttpKernel\Profiler\Profile;
 class ProfileController extends Controller
 {
 
+    public function allprofile()
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            $user->load('profileImages.mitra');
+
+            $data = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'email_verified_at' => $user->email_verified_at,
+                'bio_desc' => $user->bio_desc,
+                'image_profiles' => $user->profileImages->first()->image_profile ?? null,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+                'id_mitra' => $user->profileImages->first()->mitra->id ?? null,
+//                'image_profile' => $user->profileImages->first()->mitra->image_profile ?? null,
+                'nama' => $user->profileImages->first()->mitra->nama_lengkap ?? null,
+                'nama_toko' => $user->profileImages->first()->mitra->nama_toko ?? null,
+                'nis' => $user->profileImages->first()->mitra->nis ?? null,
+                'nomor' => $user->profileImages->first()->mitra->no_dompet_digital ?? null,
+                'image_mitra' => $user->profileImages->first()->mitra->image_id_card ?? null,
+                'status' => $user->profileImages->first()->mitra->status ?? null,
+                'pengikut' => $user->profileImages->first()->mitra->pengikut ?? null,
+                'email_dariUser' => $user->profileImages->first()->mitra->email ?? null,
+                'jumlahproduct' => $user->profileImages->first()->mitra->jumlah_product ?? null,
+                'jumlahjasa' => $user->profileImages->first()->mitra->jumlah_jasa ?? null,
+                'penilaian' => $user->profileImages->first()->mitra->penilaian ?? null,
+            ];
+
+            return response()->json([
+                "message" => "Data user berhasil didapatkan",
+                "data" => $data
+            ]);
+        }
+
+        return response()->json([
+            "message" => "User tidak terautentikasi",
+            "data" => null
+        ], 401);
+    }
+
+
     public function editProfile(Request $request)
     {
         $user = Auth::user();
