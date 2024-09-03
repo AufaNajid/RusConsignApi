6b03ba4894a2882f5b64a73ef9abe28749b8cac9
Aufa Najid <nl2221171@gmail.com>
2024-09-04 01:31:06 +0700

    fix conflict

--- app/Models/Mitra.php
+++ app/Models/Mitra.php
@@ -37,4 +37,9 @@ class Mitra extends Model
     {
         return $this->hasMany(Lokasi::class);
     }
+
+    public function profileImages()
+    {
+        return $this->hasMany(ProfileImage::class);
+    }
 }
