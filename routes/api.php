737b6a87144cb0be9440eb22c3ced8db1fb0f390
Aufa Najid <nl2221171@gmail.com>
2024-09-07 07:45:04 +0700

    fix conflict

--- routes/api.php
+++ routes/api.php
@@ -83,7 +83,7 @@ Route::group([
     Route::put('/cart/{id}', [CartController::class, 'update']);
     Route::delete('/cart/{id}', [CartController::class, 'destroy']);
     Route::post('/cart/remove-selected', [CartController::class, 'destroySelected']);
-    Route::post('/cart/select-item', [CartController::class, 'se']);
+    Route::post('/cart/select-item', [CartController::class, 'selectCartItems']);
     Route::post('/cart/checkout', [CartController::class, 'checkoutSelectedItems']);
 
 
