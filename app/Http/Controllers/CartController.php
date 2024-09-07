584cb244dbf1e2cff048f51e3278bf2ffae88561
Aufa Najid <nl2221171@gmail.com>
2024-09-07 12:10:35 +0700

    fix conflict

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -62,7 +62,7 @@ class CartController extends Controller
             $totalPrice = $barang->harga * $cart->quantity;
 
             $cartsData[] = [
-                'cart_id' => $cart->id, // Mengganti `carts_id` dengan `id`
+                'cart_id' => $cart->id, // Pastikan nama kolom yang benar
                 'quantity' => $cart->quantity,
                 'total_price' => $totalPrice, // Tambahkan total harga di sini
                 'barang' => [
@@ -99,7 +99,6 @@ class CartController extends Controller
             'carts' => $cartsData,
         ], 200);
     }
-
 
 
     public function store(Request $request)
