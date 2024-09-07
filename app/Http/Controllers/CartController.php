577fdeb96449b90f34807b1aaa787bc37a891935
Aufa Najid <nl2221171@gmail.com>
2024-09-07 08:11:38 +0700

    fix conflict

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -73,13 +73,19 @@ class CartController extends Controller
 
     public function selectCartItems(Request $request)
     {
-        $validated = $request->validate([
+        $cartIds = $request->query('cart_ids');
+
+        // Mengubah parameter menjadi array dari string
+        $cartIdsArray = explode(',', $cartIds);
+
+        // Validasi input
+        $validated = Validator::make(['cart_ids' => $cartIdsArray], [
             'cart_ids' => 'required|array',
-            'cart_ids.*' => 'integer|exists:carts,carts_id', // Pastikan nama kolom sesuai dengan database
-        ]);
+            'cart_ids.*' => 'integer|exists:carts,carts_id',
+        ])->validate();
 
         $selectedCartItems = Cart::where('user_id', Auth::id())
-            ->whereIn('carts_id', $validated['cart_ids']) // Gunakan 'carts_id' jika kolom di database
+            ->whereIn('carts_id', $validated['cart_ids'])
             ->with('barang.mitra')
             ->get();
 
@@ -92,6 +98,7 @@ class CartController extends Controller
             'selected_cart_items' => $selectedCartItems,
         ], 200);
     }
+
 
 
 
