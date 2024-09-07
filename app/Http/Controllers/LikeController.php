db414304fabdd01b8bb192d96ffad87cfb688d40
Aufa Najid <nl2221171@gmail.com>
2024-09-07 08:29:07 +0700

    fix conflict

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -18,7 +18,7 @@ class LikeController extends Controller
         }
 
         $likes = Like::where('user_id', $user->id)
-            ->with('barang.category:id,name', 'barang.mitra:id,nama_lengkap,jumlah_product,jumlah_jasa,pengikut,penilaian,no_whatsapp')
+            ->with('barang.category:id,name', 'barang.mitra:id,nama_lengkap,jumlah_product,jumlah_jasa,pengikut,penilaian,no_whatsapp, mitra.profileImage')
             ->get();
 
         if ($likes->isEmpty()) {
