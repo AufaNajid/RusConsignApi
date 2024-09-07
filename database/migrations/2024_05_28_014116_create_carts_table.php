8073268523fa3e03f16999794167ae9496cd2dd4
Aufa Najid <nl2221171@gmail.com>
2024-09-07 08:00:58 +0700

    fix conflict

--- database/migrations/2024_05_28_014116_create_carts_table.php
+++ database/migrations/2024_05_28_014116_create_carts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-//            $table->unsignedBigInteger('barang_id')->references('id')->on('barangs')->onDelete('cascade');
             $table->foreignId('barang_id')->onDelete('cascade');
             $table->foreignid('user_id');
             $table->integer('quantity')->default(1);
