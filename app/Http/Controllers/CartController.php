455f74d5fe5b2f05331b60d2f61983c5e44d15f9
Aufa Najid <nl2221171@gmail.com>
2024-09-09 08:52:38 +0700

    fix conflict

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -80,7 +80,7 @@ class CartController extends Controller
                     'updated_at' => $barang->updated_at,
                     'mitra' => [
                         'id' => $barang->mitra->id,
-                        'nama_toko' => $barang->mitra->nama_toko,
+                        'nama_toko' => $barang->mitra->name_toko,
                         'nama_lengkap' => $barang->mitra->nama_lengkap,
                         'jumlah_product' => $barang->mitra->jumlah_product,
                         'jumlah_jasa' => $barang->mitra->jumlah_jasa,
